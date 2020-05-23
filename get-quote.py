9cf469c3364a2852d9886c601564850887bc3de6
devenvansiya1984 <devenvansiya1984@gmail.com>
2020-05-23 09:04:07 +0000

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,6 +1,6 @@
 def main():
-  # print("Keep it logically awesome.")
-
+    print("Keep it logically awesome.")
+    print("Hi Deven")
   #f = open("quotes.txt")
   #quotes = f.readlines()
   #f.close()
