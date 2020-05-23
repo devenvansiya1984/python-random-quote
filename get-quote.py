6942570925782480136885c92e3e5dd5c5be3c9a
devenvansiya1984 <devenvansiya1984@gmail.com>
2020-05-23 09:56:43 +0000

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def mainT():
-    print("Keep it logically awesome.")
+    print("Responsive is better than fast")
     print("Hi Deven")
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
+    
+    print(quotes[12])
 
 if __name__== "__main__":
   mainT()
